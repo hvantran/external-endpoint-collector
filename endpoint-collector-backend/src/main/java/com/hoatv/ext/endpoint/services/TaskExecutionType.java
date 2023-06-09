7b1293a6fa13e5c7cf9c60dd0f057ae591a72090
Hoa Tran <tranvanhoactk34@gmail.com>
2023-06-09 10:46:13 +0700

    Auto close resources with try-with-resources

--- endpoint-collector-backend/src/main/java/com/hoatv/ext/endpoint/services/TaskExecutionType.java
+++ endpoint-collector-backend/src/main/java/com/hoatv/ext/endpoint/services/TaskExecutionType.java
@@ -44,27 +44,30 @@ public enum TaskExecutionType implements TaskExecutionImplementation {
                 ExtExecutionResultRepository extExecutionResultRepository = executionContext.extExecutionResultRepository;
                 EndpointExecutionResult executionResult = executionContext.executionResult;
 
-                TaskMgmtService taskMgmtExecutorV2 = TaskFactory.INSTANCE.getTaskMgmtService(noParallelThread, 5000, application);
-                GenericHttpClientPool httpClientPool = HttpClientFactory.INSTANCE.getGenericHttpClientPool(input.getTaskName(), noParallelThread, 2000);
-                for (int index = 1; index <= noAttemptTimes; index++) {
 
-                    String executionTaskName = taskName.concat(String.valueOf(index));
-                    ExtTaskEntry extTaskEntry = ExtTaskEntry.builder()
-                            .input(input)
-                            .index(index)
-                            .methodStatisticCollector(methodStatisticCollector)
-                            .httpClientPool(httpClientPool)
-                            .dataGeneratorVO(dataGeneratorVO)
-                            .filter(endpointSettingVO.getFilter())
-                            .onSuccessResponse(responseConsumer)
-                            .onErrorResponse(errorResponseConsumer)
-                            .build();
+                try (GenericHttpClientPool httpClientPool = HttpClientFactory.INSTANCE.getGenericHttpClientPool(input.getTaskName(), noParallelThread, 2000);
+                     TaskMgmtService taskMgmtExecutorV2 = TaskFactory.INSTANCE.getTaskMgmtService(noParallelThread, 5000, application)) {
 
-                    CheckedFunction<Object, TaskEntry> taskEntryFunc = TaskEntry.fromObject(executionTaskName,
-                            application);
-                    TaskEntry taskEntry = taskEntryFunc.apply(extTaskEntry);
-                    taskMgmtExecutorV2.execute(taskEntry);
-                    savePercentComplete(extExecutionResultRepository, noAttemptTimes, executionResult, index);
+                    for (int index = 1; index <= noAttemptTimes; index++) {
+
+                        String executionTaskName = taskName.concat(String.valueOf(index));
+                        ExtTaskEntry extTaskEntry = ExtTaskEntry.builder()
+                                .input(input)
+                                .index(index)
+                                .methodStatisticCollector(methodStatisticCollector)
+                                .httpClientPool(httpClientPool)
+                                .dataGeneratorVO(dataGeneratorVO)
+                                .filter(endpointSettingVO.getFilter())
+                                .onSuccessResponse(responseConsumer)
+                                .onErrorResponse(errorResponseConsumer)
+                                .build();
+
+                        CheckedFunction<Object, TaskEntry> taskEntryFunc = TaskEntry.fromObject(executionTaskName,
+                                application);
+                        TaskEntry taskEntry = taskEntryFunc.apply(extTaskEntry);
+                        taskMgmtExecutorV2.execute(taskEntry);
+                        savePercentComplete(extExecutionResultRepository, noAttemptTimes, executionResult, index);
+                    }
                 }
                 LOGGER.info("{} is completed successfully.", taskName);
                 return null;
@@ -90,13 +93,7 @@ public enum TaskExecutionType implements TaskExecutionImplementation {
                 ExtExecutionResultRepository extExecutionResultRepository = executionContext.extExecutionResultRepository;
                 EndpointExecutionResult executionResult = executionContext.executionResult;
 
-                TaskMgmtServiceV1 httpClientThreadPool = TaskFactory.INSTANCE.getTaskMgmtServiceV1(
-                        noParallelThread, 5000, application);
                 int numberOfProcess = Runtime.getRuntime().availableProcessors();
-                TaskMgmtServiceV1 cpuBoundThreadPool = TaskFactory.INSTANCE.getTaskMgmtServiceV1(numberOfProcess, 5000,
-                        "CPU-" + application);
-                GenericHttpClientPool httpClientPool = HttpClientFactory.INSTANCE.getGenericHttpClientPool(
-                        input.getTaskName(), noParallelThread, 2000);
 
                 String extEndpoint = input.getRequestInfo().getExtEndpoint();
                 String endpointMethod = input.getRequestInfo().getMethod();
@@ -105,40 +102,47 @@ public enum TaskExecutionType implements TaskExecutionImplementation {
                 String successResponseFilter = endpointSettingVO.getFilter().getSuccessCriteria();
                 HttpClientService.HttpMethod extSupportedMethod = HttpClientService.HttpMethod.valueOf(endpointMethod);
 
-                for (int index = 1; index <= noAttemptTimes; index++) {
-                    int finalIndex = index;
-                    CompletableFuture.supplyAsync(() -> {
-                                CheckedSupplier<String> supplier = () -> SaltGeneratorUtils.generateSaltValue(
-                                        dataGeneratorVO, finalIndex);
-                                ExtTaskReportVO extTaskReportVO = new ExtTaskReportVO(System.currentTimeMillis());
-                                extTaskReportVO.setAttemptValue(supplier.get());
-                                return extTaskReportVO;
-                            }, cpuBoundThreadPool)
-                            .thenApplyAsync(extTaskReportVO -> {
-                                String attemptValue = extTaskReportVO.getAttemptValue();
-                                GenericHttpClientPool.ExecutionTemplate<String> executionTemplate = ExtTaskEntry.getExecutionTemplate(
-                                        extEndpoint, extSupportedMethod, data, attemptValue, headers);
-                                String result = httpClientPool.executeWithTemplate(executionTemplate);
-                                extTaskReportVO.setExecutionResult(result);
-                                return extTaskReportVO;
-                            }, httpClientThreadPool).thenAccept(extTaskReportVO -> {
-                                String random = extTaskReportVO.getAttemptValue();
-                                String responseString = extTaskReportVO.getExecutionResult();
+                try (GenericHttpClientPool httpClientPool = HttpClientFactory.INSTANCE.getGenericHttpClientPool(input.getTaskName(), noParallelThread, 2000);
+                     TaskMgmtServiceV1 cpuBoundThreadPool = TaskFactory.INSTANCE.getTaskMgmtServiceV1(numberOfProcess, 5000,
+                             "CPU-" + application);
+                     TaskMgmtServiceV1 httpClientThreadPool = TaskFactory.INSTANCE.getTaskMgmtServiceV1(
+                             noParallelThread, 5000, application)) {
 
-                                if (StringUtils.isNotEmpty(responseString) && responseString.contains(successResponseFilter)) {
-                                    responseConsumer.accept(random, responseString);
-                                } else {
-                                    errorResponseConsumer.accept(random, responseString);
-                                }
-                                extTaskReportVO.setEndTime(System.currentTimeMillis());
-                                long elapsedTime = extTaskReportVO.getElapsedTime();
-                                methodStatisticCollector.addMethodStatistics("endpoint-processing-data-task", "ms",
-                                        elapsedTime);
-                            });
-                    savePercentComplete(extExecutionResultRepository, noAttemptTimes, executionResult, index);
+                    for (int index = 1; index <= noAttemptTimes; index++) {
+                        int finalIndex = index;
+                        CompletableFuture.supplyAsync(() -> {
+                                    CheckedSupplier<String> supplier = () -> SaltGeneratorUtils.generateSaltValue(
+                                            dataGeneratorVO, finalIndex);
+                                    ExtTaskReportVO extTaskReportVO = new ExtTaskReportVO(System.currentTimeMillis());
+                                    extTaskReportVO.setAttemptValue(supplier.get());
+                                    return extTaskReportVO;
+                                }, cpuBoundThreadPool)
+                                .thenApplyAsync(extTaskReportVO -> {
+                                    String attemptValue = extTaskReportVO.getAttemptValue();
+                                    GenericHttpClientPool.ExecutionTemplate<String> executionTemplate = ExtTaskEntry.getExecutionTemplate(
+                                            extEndpoint, extSupportedMethod, data, attemptValue, headers);
+                                    String result = httpClientPool.executeWithTemplate(executionTemplate);
+                                    extTaskReportVO.setExecutionResult(result);
+                                    return extTaskReportVO;
+                                }, httpClientThreadPool).thenAccept(extTaskReportVO -> {
+                                    String random = extTaskReportVO.getAttemptValue();
+                                    String responseString = extTaskReportVO.getExecutionResult();
+
+                                    if (StringUtils.isNotEmpty(responseString) && responseString.contains(successResponseFilter)) {
+                                        responseConsumer.accept(random, responseString);
+                                    } else {
+                                        errorResponseConsumer.accept(random, responseString);
+                                    }
+                                    extTaskReportVO.setEndTime(System.currentTimeMillis());
+                                    long elapsedTime = extTaskReportVO.getElapsedTime();
+                                    methodStatisticCollector.addMethodStatistics("endpoint-processing-data-task", "ms",
+                                            elapsedTime);
+                                });
+                        savePercentComplete(extExecutionResultRepository, noAttemptTimes, executionResult, index);
+                    }
+                    LOGGER.info("{} is completed successfully.", taskName);
+                    return null;
                 }
-                LOGGER.info("{} is completed successfully.", taskName);
-                return null;
             };
         }
     };
