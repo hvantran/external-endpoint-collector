e593c84e6d719742aba7599019a33cbc2843811f
hvantran <tranvanhoactk34@gmail.com>
2026-09-19 08:57:08 +0700

    hvantran/project-management#1 Use localhost instead of temp host name

--- endpoint-collector-backend/src/main/java/com/hoatv/ext/endpoint/config/CorsConfig.java
+++ endpoint-collector-backend/src/main/java/com/hoatv/ext/endpoint/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
 
                 registry.addMapping("/**")
                         .allowedMethods("*")
-                        .allowedOrigins("http://localhost:3001", "http://extendpointui.local:6086", "http://localhost:6086", "http://localhost:8070")
+                        .allowedOrigins("http://localhost:3001", "http://localhost:6086", "http://localhost:6086", "http://localhost:8070")
                         .allowedHeaders("*");
             }
         };
